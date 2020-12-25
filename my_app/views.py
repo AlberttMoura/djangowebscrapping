d72da01a0121cf89af0c13e67b7f46755dcf5dc9
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2020-12-25 18:11:47 -0300

    Melhorando o front-end e preparando o back-end para web o scrapping

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from . import models
 
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}&sort=rel'
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search').lower() .strip()
+    models.Search.objects.create(search=search)
+    search_url = ( '+'.join(search.split()))
+    final_url = BASE_CRAIGSLIST_URL.format(search_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'result-title'})
+    context = {
+        'search' : search,
+        'final_posting' : [],
+    }
+    return render(request, 'my_app/new_search.html', context)
